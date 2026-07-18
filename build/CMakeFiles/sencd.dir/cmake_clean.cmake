710302d86c455cf43b254ed574ffc7262faae6a5
g0ofycat <clintonusa5870@gmail.com>
2026-07-18 14:45:20 +0700

    add base

---
+++ build/CMakeFiles/sencd.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sencd.dir/link.d"
+  "CMakeFiles/sencd.dir/core/core.cpp.o"
+  "CMakeFiles/sencd.dir/core/core.cpp.o.d"
+  "sencd"
+  "sencd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sencd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
